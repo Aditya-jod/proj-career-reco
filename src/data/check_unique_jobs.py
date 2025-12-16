1b1236fc970c07bbd6104ad672bae4f5f086c131
Aditya-jod <aditya1710j@gmail.com>
2025-12-16 22:03:25 +0530

    ADDED: data cleaning file and exploration file

--- src/data/check_unique_jobs.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-try:
-    df = pd.read_csv('D:/Data Science advance projects/Career Path Recommender/Dataset/job dataset/job_descriptions.csv')
-    print(f"Total Rows: {len(df)}")
-    print(f"Unique Titles: {df['Job Title'].nunique()}")
-    print("Top 10 Titles:")
-    print(df['Job Title'].value_counts().head(10))
-except Exception as e:
-    print(e)
