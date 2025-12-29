1a588c83023a713bd4c781edd6c14cbf50533ad3
Aditya-jod <aditya1710j@gmail.com>
2025-12-29 18:49:31 +0530

    refactor: move backend code to backend/ folder and update project structure

--- src/features/nlp_utils.py
+++
@@ -1,3 +0,0 @@
-# This module contains NLP utility functions.
-# TODO: Implement skill extraction logic (using Spacy, Regex, or other libraries).
-# TODO: Add any other specific NLP helper functions needed.
